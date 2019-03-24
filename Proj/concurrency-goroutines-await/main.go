c9d737bc7d04ffe99bab1b16f91be02f55bfa6d5
thuantt0101 <thuantt01011992@gmail.com>
2019-03-24 15:09:43 +0700

    thuantt

--- Proj/concurrency-goroutines-await/main.go
+++ Proj/concurrency-goroutines-await/main.go
@@ -13,42 +13,59 @@ type Note struct {
 	Title     string
 	Completed bool
 	Creator   int
+	noteID    int
 }
 
 type User struct {
 	gorm.Model
+	Id   int
 	Name string
 }
 
+//Trả về con trỏ với kiểu dữ liệu là Struct Note và biến error
 func findNote(db *gorm.DB, id int) (*Note, error) {
 	//new ở đây là new một con trỏ
 	note := new(Note)
-	err := db.Where("id =?", id).First(&note).Error
+	//Toán tử & được sử dụng để lấy địa chỉ của một biến
+	//.First: find note
+	//tìm note trong db với id và gán cho con trỏ note
+	// Get first matched record
+
+	err := db.Where("noteID=?", id).First(&note).Error
+	//// SELECT * FROM note WHERE noteID = id limit 1;
+
 	return note, err
 }
 
 func findCreator(db *gorm.DB, id int) (*User, error) {
 	creator := new(User)
-	err := db.Where("id = ?", id).First(&creator).Error
+	err := db.Where("Id = ?", id).First(&creator).Error
 	return creator, err
 }
 
 func main() {
 	db, err := gorm.Open("mysql", "root:123456@/notes?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
-		panic(err)
+		panic("failed to connect database")
 	}
+
+	defer db.Close()
+
+	//Migrate the schema
 	db.AutoMigrate(&Note{}, &User{})
+
 	wg := new(sync.WaitGroup)
-	noteID := 1
+	//noteID := 1
 	creatorID := 1
 
-	note := new(Note)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		note, _ = findNote(db, noteID)
-	}()
+	//note := new(Note)
+	//wg.Add(1)
+
+	//go rountine
+	//go func() {
+	//	defer wg.Done()
+	//	note, _ = findNote(db, noteID)
+	//}()
 
 	creator := new(User)
 
@@ -59,6 +76,15 @@ func main() {
 	}()
 
 	wg.Wait()
-	fmt.Println("note:", note)
+
+	//fmt.Println("note:", note)
+
 	fmt.Println("creator:", creator)
+
+	if creator == nil {
+		fmt.Println("creator is nil")
+	}
+
+	fmt.Println("name:", creator.Name)
+
 }
